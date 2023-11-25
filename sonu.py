74f8b4be12e38253199340f9f61ab66b8c3effd9
sanjanadrsonu <151907179+sanjanadrsonu@users.noreply.github.com>
2023-11-25 12:27:00 +0530

    Add files via upload

---
+++ sonu.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+import string
+
+img=cv2.imread("bird.jpg")  #we have to use 'new.jpg' format images
+msg=input("enter the secret message:")
+passcord= input("enter the passcord:")
+
+d={}
+c={}
+
+for i in range(255):
+    d[chr(i)]=i
+    c[i]=chr(i)
+
+n=0
+m=0
+z=0 
+
+for i in range(len(msg)):
+    img[n,m,z]=d[msg[i]]
+    n=n+1
+    m=m+1
+    z=(z+1)%3
+
+cv2.imwrite("encryptedImage.jpg",img)
+os.startfile("encryptedImage.jpg")
+message=""
+n=0
+m=0
+z=0
+    
+pas=input("enter passcode for Decryption:")
+if(passcord==pas):
+    for i in range(len(msg)):
+        message=message+c[img[n,m,z]]
+        n=n+1
+        m=m+1
+        z=(z+1)%3
+    print("decryped message=",message)
+else:
+    print("you are not a Authenticated")
